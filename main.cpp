241ca89756136f62d02f1ac9503cb587d653f65d
Kohei Noda <k.noda@saver.jp>
2024-10-10 12:59:30 +0900

    initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iphlpapi.h>
+#include <winsock2.h>
+
+int main() {
+    auto             buffer_size   = ULONG(sizeof(IP_ADAPTER_INFO));
+    IP_ADAPTER_INFO* adapter_info = (IP_ADAPTER_INFO*)malloc(buffer_size);
+    if(GetAdaptersInfo(adapter_info, &buffer_size) == ERROR_BUFFER_OVERFLOW) {
+        free(adapter_info);
+        adapter_info = (IP_ADAPTER_INFO*)malloc(buffer_size);
+    }
+
+    if(GetAdaptersInfo(adapter_info, &buffer_size) == NO_ERROR) {
+        while(adapter_info) {
+            std::cout << "Adapter Name: " << adapter_info->AdapterName << std::endl;
+            std::cout << "IP Address: " << adapter_info->IpAddressList.IpAddress.String << std::endl;
+            adapter_info = adapter_info->Next;
+        }
+    }
+    return 0;
+}
